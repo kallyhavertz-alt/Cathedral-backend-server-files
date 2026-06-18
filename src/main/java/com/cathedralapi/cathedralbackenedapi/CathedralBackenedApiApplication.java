1aad8eda9bcd5152f66fcb35bde9bf003e5b6e7c
kallyhavertz-alt <kallyhavertz@gmail.com>
2026-06-18 22:59:06 +0300

    security

--- src/main/java/com/cathedralapi/cathedralbackenedapi/CathedralBackenedApiApplication.java
+++ src/main/java/com/cathedralapi/cathedralbackenedapi/CathedralBackenedApiApplication.java
@@ -1,16 +1,31 @@
 package com.cathedralapi.cathedralbackenedapi;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CathedralBackenedApiApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Protects and allows all API path patterns
+                        .allowedOrigins("*") // Lets any mobile IP address connect
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
     public static void main(String[] args) {
         SpringApplication.run(CathedralBackenedApiApplication.class, args);
