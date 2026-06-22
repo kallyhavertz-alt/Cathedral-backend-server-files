be66d0f8ca73d054b5d933e2a53342ee9d936b3f
kallyhavertz-alt <kallyhavertz@gmail.com>
2026-06-22 15:31:33 +0300

    import

--- src/main/java/com/cathedralapi/cathedralbackenedapi/StaffNoticeRepository.java
+++ src/main/java/com/cathedralapi/cathedralbackenedapi/StaffNoticeRepository.java
@@ -3,6 +3,7 @@ package com.cathedralapi.cathedralbackenedapi;
 import com.cathedralapi.cathedralbackenedapi.StaffNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+//imports end
 
 public interface StaffNoticeRepository extends JpaRepository<StaffNotice, Long> {
     List<StaffNotice> findAllByOrderByCreatedAtDesc();
